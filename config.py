3f3cdd14fbb0cb06883795c3fb521af7ec54daf9
Jeff Walker <walker.jeff.d@gmail.com>
2014-06-10 19:33:04 -0400

    change requirements for heroku, add folder paths to heroku config

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ class Config:
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ABCDEFGHIJKLMNOP'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    QPF_TAR_FOLDER = os.path.join(basedir, 'data', 'tar')
-    QPF_SHP_FOLDER = os.path.join(basedir, 'data', 'shp')
 
     @staticmethod
     def init_app(app):
@@ -16,12 +14,19 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    QPF_TAR_FOLDER = os.path.join(basedir, 'data', 'tar')
+    QPF_SHP_FOLDER = os.path.join(basedir, 'data', 'shp')
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'open-water.sqlite')
+    QPF_TAR_FOLDER = os.path.join(basedir, 'data', 'tar')
+    QPF_SHP_FOLDER = os.path.join(basedir, 'data', 'shp')
 
 class HerokuConfig(ProductionConfig):
+    QPF_TAR_FOLDER = os.path.join(basedir, 'tmp', 'tar')
+    QPF_SHP_FOLDER = os.path.join(basedir, 'tmp', 'shp')
+
     @classmethod
     def init_app(cls, app):
         ProductionConfig.init_app(app)
